52490e136985f292d71861e43cdb65de162848d0
Brian Hsieh <brianh0702@gmail.com>
2021-11-03 11:06:43 -0700

    Added login.html and firebaseAPI.js

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyCclb41nkpYfiv5jqykeZN3y_qXNIPFrKA",
+  authDomain: "comp1800-bby14.firebaseapp.com",
+  projectId: "comp1800-bby14",
+  storageBucket: "comp1800-bby14.appspot.com",
+  messagingSenderId: "52513653092",
+  appId: "1:52513653092:web:02e430ef55395d5249fa71",
+};
+
+
+const app = firebase.initializeApp(firebaseConfig);
